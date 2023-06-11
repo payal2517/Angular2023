0d731bcc67c850a7345e6610b536a8ca18d54100
Payal Shigwan <pshigwan@eisinc.com>
2023-06-11 19:48:30 +0530

    First Project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/restaurants'),
+      dir: require('path').join(__dirname, './coverage/RestaurentApp'),
       subdir: '.',
       reporters: [
         { type: 'html' },
